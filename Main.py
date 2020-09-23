0f5903a30034beb85388c876daba189f65e1917e
w0rt0x <61494137+w0rt0x@users.noreply.github.com>
2020-09-23 10:16:14 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,5 +1,5 @@
 from WebApp import app
 
 if __name__ == '__main__':
-    app.run(port=5000, debug=False, threaded=True)  # Threading makes Multiple Users at once possible
+    app.run(host= '0.0.0.0', port=5000, debug=False, threaded=True)  # Threading makes Multiple Users at once possible
     # host= '0.0.0.0'
