3a9d9d4780cda7c2b465049b3273b0c8d173f504
w0rt0x <t.filzer@gmx.de>
2021-03-08 16:19:40 +0100

    Reads are now tested too

--- Results/Strain-Typing/StratifiedKFold.py
+++ Results/Strain-Typing/StratifiedKFold.py
@@ -109,16 +109,22 @@ def get_read_data(file, dic, keys):
     """ Assigns reads to labels"""
     r = csv.reader(open(file))
     lines = list(r)
-    vecs = []
-    labels = []
+    vecs_forwards = []
+    labels_forwards = []
+    vecs_reverse = []
+    labels_reverse = []
     for key in keys:
         for i in dic[key]:
             for j in lines:
                 if i in j[0]:
-                    vecs.append(j[2:])
-                    labels.append(key)
+                    if '_2.fq' in j[0] or '_R2_' in j[0]:
+                        vecs_reverse.append(j[2:])
+                        labels_reverse.append(key)
+                    else:
+                        vecs_forwards.append(j[2:])
+                        labels_forwards.append(key)
 
-    return np.array(vecs), np.array(labels)
+    return np.array(vecs_forwards), np.array(labels_forwards), np.array(vecs_reverse), np.array(labels_reverse)
 
 
 def print_stats(acc):
@@ -192,6 +198,7 @@ def create_plots():
     print('Please Note: Data from Quick-run (only every 10th k-mer is tested) is used for testing only, meaning that the'
           'trainingdata is taken from the k-folded Full-run, while the quick-run is used for testing only.')
     print(' ')
+
     # Trying all possible Combinations
 
     # Assembly reference
@@ -277,22 +284,99 @@ def create_plots():
     print_stats(acc)
 
     # Testing Sequence Reads
+    ICs_assembly = get_ICs()
+    ICs_core = get_ICs()
+    forward, forward_label, reverse, reverse_label = get_read_data('Reads as Input, Assembly as Reference.csv',
+                                                                   ICs_assembly, keys)
+
     print('StratifiedKFold (n=10): Assembled genomes as reference, unmodified forward reads')
+    acc = []
+    for train_index, test_index in skf.split(assembly_full, assembly_full_label):
+        X_train, X_test = assembly_full[train_index], assembly_full[test_index]
+        y_train, y_test = assembly_full_label[train_index], assembly_full_label[test_index]
+
+        acc.append(get_cm_and_accuracy(X_train, y_train, forward, forward_label, 'poly'))
+
+    print_stats(acc)
 
     print('StratifiedKFold (n=10): Assembled genomes as reference, unmodified reverse reads')
+    acc = []
+    for train_index, test_index in skf.split(assembly_full, assembly_full_label):
+        X_train, X_test = assembly_full[train_index], assembly_full[test_index]
+        y_train, y_test = assembly_full_label[train_index], assembly_full_label[test_index]
 
+        acc.append(get_cm_and_accuracy(X_train, y_train, reverse, reverse_label, 'poly'))
+
+    print_stats(acc)
+
+    forward, forward_label, reverse, reverse_label = get_read_data('Reads as Input, Assembly as Reference, 15% of the nucleotides changed.csv',
+                                                                   ICs_assembly, keys)
     print('StratifiedKFold (n=10): Assembled genomes as reference, forward reads with 15% randomly changed nucleotides')
+    acc = []
+    for train_index, test_index in skf.split(assembly_full, assembly_full_label):
+        X_train, X_test = assembly_full[train_index], assembly_full[test_index]
+        y_train, y_test = assembly_full_label[train_index], assembly_full_label[test_index]
+
+        acc.append(get_cm_and_accuracy(X_train, y_train, forward, forward_label, 'poly'))
+
+    print_stats(acc)
 
     print('StratifiedKFold (n=10): Assembled genomes as reference, reverse reads with 15% randomly changed nucleotides')
+    acc = []
+    for train_index, test_index in skf.split(assembly_full, assembly_full_label):
+        X_train, X_test = assembly_full[train_index], assembly_full[test_index]
+        y_train, y_test = assembly_full_label[train_index], assembly_full_label[test_index]
 
+        acc.append(get_cm_and_accuracy(X_train, y_train, reverse, reverse_label, 'poly'))
+
+    print_stats(acc)
+
+    # Core Genome Reference
+    forward, forward_label, reverse, reverse_label = get_read_data('Reads as Input, Assembly as Reference.csv',
+                                                                   ICs_core, keys)
     print('StratifiedKFold (n=10): Core Genome as reference, unmodified forward reads')
+    acc = []
+    for train_index, test_index in skf.split(core_full, core_full_label):
+        X_train, X_test = core_full[train_index], core_full[test_index]
+        y_train, y_test = core_full_label[train_index], core_full_label[test_index]
+
+        acc.append(get_cm_and_accuracy(X_train, y_train, forward, forward_label, 'poly'))
+
+    print_stats(acc)
 
     print('StratifiedKFold (n=10): Core Genome as reference, unmodified reverse reads')
+    acc = []
+    for train_index, test_index in skf.split(core_full, core_full_label):
+        X_train, X_test = core_full[train_index], core_full[test_index]
+        y_train, y_test = core_full_label[train_index], core_full_label[test_index]
 
+        acc.append(get_cm_and_accuracy(X_train, y_train, reverse, reverse_label, 'poly'))
+
+    print_stats(acc)
+
+
+    forward, forward_label, reverse, reverse_label = get_read_data(
+        'Reads as Input, Core-Genome as Reference, 15% of the nucleotides changed.csv',
+        ICs_assembly, keys)
     print('StratifiedKFold (n=10): Core Genome as reference, forward reads with 15% randomly changed nucleotides')
+    acc = []
+    for train_index, test_index in skf.split(core_full, core_full_label):
+        X_train, X_test = core_full[train_index], core_full[test_index]
+        y_train, y_test = core_full_label[train_index], core_full_label[test_index]
+
+        acc.append(get_cm_and_accuracy(X_train, y_train, forward, forward_label, 'poly'))
+
+    print_stats(acc)
 
     print('StratifiedKFold (n=10): Core Genome as reference, reverse reads with 15% randomly changed nucleotides')
+    acc = []
+    for train_index, test_index in skf.split(core_full, core_full_label):
+        X_train, X_test = core_full[train_index], core_full[test_index]
+        y_train, y_test = core_full_label[train_index], core_full_label[test_index]
 
+        acc.append(get_cm_and_accuracy(X_train, y_train, reverse, reverse_label, 'poly'))
+
+    print_stats(acc)
 
 create_plots()
 
